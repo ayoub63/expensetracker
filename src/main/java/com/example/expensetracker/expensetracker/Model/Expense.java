401a43cbda60057997c74ed1bd6898038f43fe68
ayoub63 <ayoubafkir18@gmail.com>
2025-01-29 22:30:03 +0100

    feature: Expense Model, fix: pom.xml

---
+++ src/main/java/com/example/expensetracker/expensetracker/Model/Expense.java
@@ -0,0 +1,61 @@
+package com.example.expensetracker.expensetracker.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double amount;
+    private String description;
+
+    // Constructors
+    public Expense() {}
+    
+    public Expense(String name, Double amount, String description) {
+        this.name = name;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
