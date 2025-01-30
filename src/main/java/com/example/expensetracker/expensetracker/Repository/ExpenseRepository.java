8d9f51041eb245dd3f09dfe1f51aa5a9aa56f947
ayoub63 <ayoubafkir18@gmail.com>
2025-01-30 18:34:38 +0100

    added dependencies in package.json

--- src/main/java/com/example/expensetracker/expensetracker/Repository/ExpenseRepository.java
+++ src/main/java/com/example/expensetracker/expensetracker/Repository/ExpenseRepository.java
@@ -1,8 +1,9 @@
 package com.example.expensetracker.expensetracker.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.example.expensetracker.expensetracker.Model.Expense;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-
 }
