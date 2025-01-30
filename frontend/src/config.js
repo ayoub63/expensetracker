95ad65faa0e73755cdf9171495b8cd694d972269
ayoub63 <ayoubafkir18@gmail.com>
2025-01-30 23:02:32 +0100

    .

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
+console.log('API URL:', API_URL); // For debugging
+if (!API_URL) {
+    throw new Error('REACT_APP_API_URL is not defined');
+}
 export default API_URL; 
