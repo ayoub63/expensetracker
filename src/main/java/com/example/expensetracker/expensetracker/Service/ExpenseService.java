e67bc2b97077b767798c6fa9d61b3686a9ef1de0
ayoub63 <ayoubafkir18@gmail.com>
2025-01-30 11:46:12 +0100

    ExpenseService Class (save and get)

---
+++ src/main/java/com/example/expensetracker/expensetracker/Service/ExpenseService.java
@@ -0,0 +1,23 @@
+package com.example.expensetracker.expensetracker.Service;
+
+import com.example.expensetracker.expensetracker.Model.Expense;
+import com.example.expensetracker.expensetracker.Repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseService {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public Expense saveExpense(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+}
